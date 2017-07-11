1a45f489a40a0d7647dc499375bb2d8710432d91
Zhou Yuan <1693081@CEGEPGIM.LCL>
2017-07-11 16:08:33 -0400

    commit2

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    void Update()
+    {
+        Animator anim = GetComponent<Animator>();
+        if (Input.GetKeyDown("space"))
+        {
+            anim.SetTime(0);
+            anim.Play("JUMP00");
+        }
+    }
+}
